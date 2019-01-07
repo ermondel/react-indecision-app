8aa54424a3cb72ca98ede9197f4547675f154e82
ermondel <ermondel@gmail.com>
2019-01-07 20:06:49 +0200

    feat: add some simple examples of jsx rendering to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-let template = <h1>Indecision App</h1>;
+let app = {
+    title: 'Indecision App',
+    options: [],
+};
+
+let template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options.length > 0 ? "Here are your options" : "No options"}
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+let user = {
+    name: 'Terry',
+    age: 46,
+    location: 'Cambridge'
+};
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>
+    }
+}
+
+let templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
 
 let appRoot = document.getElementById('app');
 
