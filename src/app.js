4578f5930b1cdbe9322dce75672e9b99843037df
ermondel <ermondel@gmail.com>
2019-01-16 18:55:05 +0200

    feat: add the form with the submit handler to enter an option name

--- src/app.js
+++ src/app.js
@@ -5,46 +5,37 @@ const app = {
     options: [],
 };
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        {app.options.length > 0 ? "Here are your options" : "No options"}
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
-
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-}
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-const reset = () => {
-    count = 0;
-    renderCounterApp();
+const onFormSubmit = event => {
+    event.preventDefault();
+    const option = event.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        event.target.elements.option.value = '';
+        render();
+    }
 }
 
 const appRoot = document.getElementById('app');
 
-//
-const renderCounterApp = () => {
-    const templateTwo = (
+const render = () => {
+    const template = (
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add Option</button>
+            </form>
         </div>
     );
 
-    ReactDOM.render(templateTwo, appRoot);
+    ReactDOM.render(template, appRoot);
 }
 
-renderCounterApp();
+render();
