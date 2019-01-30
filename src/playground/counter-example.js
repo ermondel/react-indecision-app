c4e6ec3d60e82dfbcc114a5d32d6ec52f47b705e
ermondel <ermondel@gmail.com>
2019-01-30 15:58:50 +0200

    exercise: default prop values

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset    = this.handleReset.bind(this);
         this.state = {
-            count: 0
+            count: props.count
         }
     }
     handleAddOne() {
@@ -40,37 +40,8 @@ class Counter extends React.Component {
         );
     }
 }
+Counter.defaultProps = {
+    count: 0
+}
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
-
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// }
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// }
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// }
-
-// const appRoot = document.getElementById('app');
-
-// //
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reset}>Reset</button>
-//         </div>
-//     );
-
-//     ReactDOM.render(templateTwo, appRoot);
-// }
-
-// renderCounterApp();
