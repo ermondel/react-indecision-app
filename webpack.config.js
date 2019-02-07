509bcfa8ee7aa467a8a21e34efe2d59cadd95074
ermondel <ermondel@gmail.com>
2019-02-07 14:06:42 +0200

    exercise: setting up babel with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
     }
 }
