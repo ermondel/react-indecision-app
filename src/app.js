7cd7dc366854c70e9822180293680df3cba62356
ermondel <ermondel@gmail.com>
2019-02-07 11:50:34 +0200

    exercise: default exports

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-import { square, add } from './utils';
-import { isAdult, canDrink } from './person';
+import subtract, { square, add } from './utils';
+import isSenior, { isAdult, canDrink } from './person';
 
 console.log('app.js is running !');
+
 console.log('square', square(5));
 console.log('add', add(200, 25));
+console.log('subtract', subtract(100, 77));
 console.log('isAdult', isAdult(30));
 console.log('canDrink', canDrink(30));
+console.log('isSenior', isSenior(63));
--- src/person.js
+++ src/person.js
@@ -1,2 +1,4 @@
 export const isAdult = (age) => age >=18;
 export const canDrink = (age) => age >=21;
+const isSenior = (age) => age >= 64;
+export default isSenior;
