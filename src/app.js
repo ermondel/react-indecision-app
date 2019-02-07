ac228aa8083d103745877b28099f7cc3ee7c5ac4
ermondel <ermondel@gmail.com>
2019-02-07 10:56:19 +0200

    exercise: es6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+import { square, add } from './utils';
+import { isAdult, canDrink } from './person';
+
 console.log('app.js is running !');
+console.log('square', square(5));
+console.log('add', add(200, 25));
+console.log('isAdult', isAdult(30));
+console.log('canDrink', canDrink(30));
