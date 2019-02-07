6e4e25d6739216679273ee513bbb8c0eed2992e9
ermondel <ermondel@gmail.com>
2019-02-07 13:26:28 +0200

    exercise: importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-import subtract, { square, add } from './utils';
-import isSenior, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running !');
-
-console.log('square', square(5));
-console.log('add', add(200, 25));
-console.log('subtract', subtract(100, 77));
-console.log('isAdult', isAdult(30));
-console.log('canDrink', canDrink(30));
-console.log('isSenior', isSenior(63));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
