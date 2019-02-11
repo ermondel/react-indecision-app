ec917be79b6e72f6bed734a636a1ad7cd21d6eca
ermondel <ermondel@gmail.com>
2019-02-11 14:22:44 +0200

    exercise: big button and options list

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Option = (props) => (
     <div>
         {props.optionText}
-        <button onClick={() => props.handleDeleteOption(props.optionText)}>remove</button>
+        <button 
+            className="button button--link"
+            onClick={() => props.handleDeleteOption(props.optionText)}
+        >remove</button>
     </div>
 );
 
