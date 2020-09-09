efa9ed9393e274b1c88b2c9f82717f36034e2a1f
ermondel <ermondel@gmail.com>
2020-09-09 18:04:34 +0300

    fix: style: code formatting and favicon

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,28 +2,25 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
-    <div>
-        <div className="widget-header">
-            <h3 className="widget-header__title">Your Options</h3>
-            <button 
-                className="button button--link"
-                onClick={props.handleDeleteOptions} 
-            >
-                Remove All
-            </button>
-        </div>
-        { props.options.length === 0 && <p className="widget-message">Please add an option to get started!</p> }
-        { 
-            props.options.map((option, index) => (
-                <Option 
-                    optionText={option} 
-                    key={option} 
-                    handleDeleteOption={props.handleDeleteOption}
-                    count={index + 1}
-                />
-            ))
-        }
+  <div>
+    <div className='widget-header'>
+      <h3 className='widget-header__title'>Your Options</h3>
+      <button className='button button--link' onClick={props.handleDeleteOptions}>
+        Remove All
+      </button>
     </div>
+    {props.options.length === 0 && (
+      <p className='widget-message'>Please add an option to get started!</p>
+    )}
+    {props.options.map((option, index) => (
+      <Option
+        optionText={option}
+        key={option}
+        handleDeleteOption={props.handleDeleteOption}
+        count={index + 1}
+      />
+    ))}
+  </div>
 );
 
 export default Options;
