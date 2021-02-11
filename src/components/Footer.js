466bb67fa9b879dee45a3f7c910eab8dad053163
ermondel <ermondel@gmail.com>
2021-02-11 20:37:39 +0200

    feat: add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Footer = () => {
+  let footerYear = 2019;
+  const currentYear = new Date().getFullYear();
+
+  if (footerYear !== currentYear) {
+    footerYear = `${footerYear} - ${currentYear}`;
+  }
+
+  return (
+    <footer className='footer'>
+      <span className='footer__sign'>&copy;</span>{' '}
+      <span className='footer__year'>{footerYear}</span>{' '}
+      <a
+        href='https://github.com/ermondel'
+        target='_blank'
+        rel='noopener noreferrer'
+        title='All rights reserved | created by Serhii (ermondel)'
+        className='footer__link'
+      >
+        Serhii
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import Footer from './components/Footer';
 import './styles/index.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(
+  <React.Fragment>
+    <IndecisionApp />
+    <Footer />
+  </React.Fragment>,
+  document.getElementById('app')
+);
