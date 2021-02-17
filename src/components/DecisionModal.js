7910f5ac63244563d7bc581d1a3bd6bf6cce221f
ermondel <ermondel@gmail.com>
2021-02-17 16:33:18 +0200

    feat: responsive design

--- src/components/DecisionModal.js
+++ src/components/DecisionModal.js
@@ -5,11 +5,11 @@ const DecisionModal = React.memo((props) => (
   <Modal
     isOpen={props.isOpen}
     onRequestClose={props.closeModal}
-    contentLabel='Selected Option'
+    contentLabel='Selected Decision'
     closeTimeoutMS={200}
     className='modal'
   >
-    <h3 className='modal__title'>Selected Option</h3>
+    <h3 className='modal__title'>Selected Decision</h3>
     <p className='modal__body'>{props.decision}</p>
     <button className='modal__close-btn' onClick={props.closeModal}>
       Ok
