137ad26ff19fb3200bdc05de2cba66bf2902a73a
ermondel <ermondel@gmail.com>
2021-02-27 15:46:12 +0200

    feat: add a snapshot test of the entire app

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import '../styles/index.scss';
 
 const App = () => (
   <React.Fragment>
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import IndecisionApp from '../components/IndecisionApp';
+import App from '../components/App';
 import Modal from 'react-modal';
 
 jest.spyOn(Modal, "setAppElement").mockImplementation(() => {});
 
-it('<IndecisionApp />', () => {
-  const tree = renderer.create(<IndecisionApp />).toJSON();
+it('<App />', () => {
+  const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
